d38c264042599233cfa41450606299520189e0bc
singhVikram97 <vikram.bedi97@gmail.com>
2018-05-27 09:16:45 +0530

    Simple whiteboard without socket

---
+++ public/Js/index.js
@@ -0,0 +1,50 @@
+const socket=io();
+
+$(document).ready(function () {
+
+    const canvas=document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
+
+    let current={};
+    let drawing=false;
+    
+    
+    function drawLine(x1,y1,x2,y2){
+
+        if(!drawing){
+            return;
+        }
+
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+        context.closePath();
+    }
+
+    canvas.addEventListener('mousedown',onMouseDown);
+    canvas.addEventListener('mouseup',onMouseUp);
+    canvas.addEventListener('mousemove',onMouseMove);
+    canvas.addEventListener('mouseout',onMouseUp); // Stop drawing same as mouseUp
+    
+    function onMouseDown(e) {
+        current.x1=e.clientX;
+        current.y1=e.clientY;
+        drawing=true;
+    }
+
+    function onMouseUp(e) {
+        drawLine(current.x1,current.y1,e.clientX,e.clientY);
+        drawing=false;
+    }
+
+    function onMouseMove(e) {
+        drawLine(current.x1,current.y1,e.clientX,e.clientY);
+        current.x1=e.clientX;
+        current.y1=e.clientY;
+    }
+
+});
